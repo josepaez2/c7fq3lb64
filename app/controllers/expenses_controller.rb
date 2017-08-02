87c7d494aa9b43f60c8c1556107112189d8ec1bd
josepaez2 <josepaez_2@hotmail.com>
2017-08-01 20:53:10 -0500

    final

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,22 @@
 class ExpensesController < ApplicationController
+  # def index
+  #    @expenses = Expense.order("date DESC")
+  # end
+
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
+      @expenses = @expenses.where(category_id: params[:category_id])
+    elsif params[:category_id].present?
+      @expenses = @expenses.where(category_id: params[:category_id])
+    elsif params[:concept].present?
+      @expenses = @expenses.where("concept LIKE :q", q: "%#{params[:concept]}%")
+    end
+    respond_to do |format|
+      format.html
+      format.js
+      format.json { render json: @responses }
+    end
   end
 end

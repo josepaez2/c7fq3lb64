4938742c7ddb53bd21008d0f034b340f02fc451f
josepaez2 <josepaez_2@hotmail.com>
2017-08-09 18:45:09 -0500

    sin oath

--- Gemfile
+++ Gemfile
@@ -23,7 +23,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
   # bundle exec rake doc:rails generates the API under doc/api.     # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+# Perform jobs
+gem 'sidekiq'
 
 group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console       # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -36,4 +37,6 @@ group :development do
   gem 'spring'
   # Ver los mails que va mandar
   gem 'letter_opener'
+  # Perform jobs
+  gem 'sidekiq'
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -23,7 +23,41 @@ before_action :authenticate_user!
 		UserMailer.welcome(@current_user, @expenses).deliver_now
 		# UserMailer.welcome(@current_user).deliver_later
 		# se hace con background job
-		
+
 		# flash[:success] = "Has iniciado sesión"
   end
+
+  # def new
+  #   @person = Person.new
+  #   2.times do
+  #     @person.addresses.build
+  #   end
+  # end
+
+  # def show
+  #   @person = Person.find(params[:id])
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def create
+  #   @person = Person.create!(person_params)
+  #   respond_to do |format|
+  #     format.html { redirect_to people_path}
+  #     format.json { render json: person}
+  #     format.js
+  #   end
+  # end
+
+  # private
+
+  # def person_params
+  #   params.require(:person).permit(:name, :email, addresses_attributes: [:city, :street])
+  # end
+
+  
 end
